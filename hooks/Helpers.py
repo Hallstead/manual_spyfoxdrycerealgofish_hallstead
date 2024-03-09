501184455d377306d04e10262abb8fb4e19a6106
BuildTools <unconfigured@null.spigotmc.org>
2024-03-08 20:27:48 -0600

    made number of Win Game locations optional and removed traps

--- hooks/Helpers.py
+++ hooks/Helpers.py
@@ -1,8 +1,29 @@
 from typing import Optional
 from BaseClasses import MultiWorld
+from .. import Helpers
 
 
 # Use this if you want to override the default behavior of is_option_enabled
 # Return True to enable the category, False to disable it, or None to use the default behavior
 def before_is_category_enabled(world: MultiWorld, player: int, category_name: str) -> Optional[bool]:
+    if category_name == "G1":
+        if Helpers.get_option_value(world, player, "NumGames") >= 1:
+            return True
+        return False
+    if category_name == "G2":
+        if Helpers.get_option_value(world, player, "NumGames") >= 2:
+            return True
+        return False
+    if category_name == "G3":
+        if Helpers.get_option_value(world, player, "NumGames") >= 3:
+            return True
+        return False
+    if category_name == "G4":
+        if Helpers.get_option_value(world, player, "NumGames") >= 4:
+            return True
+        return False
+    if category_name == "G5":
+        if Helpers.get_option_value(world, player, "NumGames") >= 5:
+            return True
+        return False
     return None
--- hooks/Options.py
+++ hooks/Options.py
@@ -26,16 +26,17 @@ from ..Helpers import is_option_enabled, get_option_value
 # To add an option, use the before_options_defined hook below and something like this:
 #   options["total_characters_to_win_with"] = TotalCharactersToWinWith
 #
-class TotalCharactersToWinWith(Range):
-    """Instead of having to beat the game with all characters, you can limit locations to a subset of character victory locations."""
-    display_name = "Number of characters to beat the game with before victory"
-    range_start = 10
-    range_end = 50
-    default = 50
+class NumGames(Range):
+    """How many "win game" locations to include in the pool"""
+    display_name = "Number of 'Win X Games' locations?"
+    range_start = 0
+    range_end = 5
+    default = 3
 
 
 # This is called before any manual options are defined, in case you want to define your own with a clean slate or let Manual define over them
 def before_options_defined(options: dict) -> dict:
+    options["NumGames"] = NumGames
     return options
 
 # This is called after any manual options are defined, in case you want to see what options are defined or want to modify the defined options
